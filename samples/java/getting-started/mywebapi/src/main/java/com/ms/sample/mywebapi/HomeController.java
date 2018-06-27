64a87050f5b835355a70f774a0a6bd9562dffcfb
andxu <andxu@microsoft.com>
2018-06-27 15:09:31 +0800

    remove a uesless import

--- samples/java/getting-started/mywebapi/src/main/java/com/ms/sample/mywebapi/HomeController.java
+++ samples/java/getting-started/mywebapi/src/main/java/com/ms/sample/mywebapi/HomeController.java
@@ -2,7 +2,6 @@ package com.ms.sample.mywebapi;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 public class HomeController {
