fe58f849ad2ec894903b779671ac193ad43f45d6
andxu <andxu@microsoft.com>
2018-06-27 14:58:30 +0800

    initial commit for mywebapi java example

---
+++ samples/java/getting-started/mywebapi/src/main/java/com/ms/sample/mywebapi/HomeController.java
@@ -0,0 +1,19 @@
+package com.ms.sample.mywebapi;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Hello from Spring Boot!";
+    }
+
+    @RequestMapping("/version")
+    public String version() {
+        return "0.1.0";
+    }
+}   
