7cc8553e13b55dee3d2190a2d76bc6e33e554d80
Saurabh Sarkar <saurabsa@microsoft.com>
2018-10-25 16:22:15 -0700

    Updating the logic

--- samples/golang/getting-started/webfrontend/src/app/main.go
+++ samples/golang/getting-started/webfrontend/src/app/main.go
@@ -9,51 +9,48 @@ import (
 	"strconv"
 )
 
-// serverMessage is a representation of a message from server
-type serverMessage struct {
-	Value string
-}
-
 // pageVariables is a representation of a server page
 type pageVariables struct {
-	PageTitle   string
-	PageMessage serverMessage
-	PageButton  string
+	PageTitle      string
+	PageButton     string
+	TargetPort     int
+	DefaultCounter int
 }
 
 var counter int
+var targetPort int
 
 func main() {
+	counter = 0
 	http.HandleFunc("/", HomePage)
+	http.HandleFunc("/counter", ManageCounter)
 	http.HandleFunc("/api", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello from webfrontend")
 	})
+
+	// Start the server at http://localhost:80
 	listener, err := net.Listen("tcp", ":80")
 	if err != nil {
 		log.Fatal("Listen failed: ", err)
 	}
-	fmt.Println("Server is listening on port", listener.Addr().(*net.TCPAddr).Port)
+	targetPort = listener.Addr().(*net.TCPAddr).Port
+	fmt.Println("Server is listening on port", targetPort)
 	log.Fatal(http.Serve(listener, nil))
 }
 
 // HomePage is triggered on load of the server
 func HomePage(w http.ResponseWriter, r *http.Request) {
 	title := "Server"
-	myButton := "Click Me (Reload)"
-	messages := make(chan string)
-	go func() {
-		counter++
-		messages <- strconv.Itoa(counter)
-	}()
-	myMessage := serverMessage{<-messages}
-
+	myButton := "Click Me"
 	myPageVariables := pageVariables{
-		PageTitle:   title,
-		PageMessage: myMessage,
-		PageButton:  myButton,
+		PageTitle:      title,
+		PageButton:     myButton,
+		TargetPort:     targetPort,
+		DefaultCounter: counter,
 	}
 
-	t, err := template.ParseFiles("index.html") //parse the html file homepage.html
+	// Load the index.html template.
+	t, err := template.ParseFiles("index.html") //parse the html file index.html
 	if err != nil {                             // if there is an error
 		log.Print("template parsing error: ", err) // log it
 	}
@@ -61,4 +58,15 @@ func HomePage(w http.ResponseWriter, r *http.Request) {
 	if err != nil {                     // if there is an error
 		log.Print("template executing error: ", err) //log it
 	}
+}
+
+// ManageCounter is triggered on click of the button
+func ManageCounter(w http.ResponseWriter, r *http.Request) {
+	messages := make(chan string)
+	go func() {
+		counter++
+		messages <- strconv.Itoa(counter)
+	}()
+	myMessage := <-messages
+	fmt.Fprintln(w, myMessage)
 }
