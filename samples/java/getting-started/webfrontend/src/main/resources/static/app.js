16cb4c0a2f403f0112c163cb5b64bcb9d745a5b9
daniv-msft <48293037+daniv-msft@users.noreply.github.com>
2019-04-18 09:24:41 -0700

    Add copyright headers on samples

--- samples/java/getting-started/webfrontend/src/main/resources/static/app.js
+++ samples/java/getting-started/webfrontend/src/main/resources/static/app.js
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 var app = angular.module('myApp', ['ngRoute']);
 
 app.controller('MainController', function($scope, $http) {
--- samples/nodejs/getting-started/mywebapi/server.js
+++ samples/nodejs/getting-started/mywebapi/server.js
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 var express = require('express');
 var app = express();
 
