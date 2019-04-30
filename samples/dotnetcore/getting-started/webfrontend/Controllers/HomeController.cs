e0898212fc727af46142c3f9a5d7c3929c565129
daniv-msft <48293037+daniv-msft@users.noreply.github.com>
2019-04-30 09:01:48 -0700

    Add usings needed for quickstart so that users don't have to add them themselves

--- samples/dotnetcore/getting-started/webfrontend/Controllers/HomeController.cs
+++ samples/dotnetcore/getting-started/webfrontend/Controllers/HomeController.cs
@@ -1,7 +1,10 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using webfrontend.Models;
 
