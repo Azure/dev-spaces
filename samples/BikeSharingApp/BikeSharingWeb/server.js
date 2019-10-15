fae25fea5d42523b02c1bfc113332a6dc60f6276
Anupama Tuli <anutuli@microsoft.com>
2019-10-15 16:09:39 -0700

    trig

--- samples/BikeSharingApp/BikeSharingWeb/server.js
+++ samples/BikeSharingApp/BikeSharingWeb/server.js
@@ -10,6 +10,7 @@ const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
+
 app.prepare()
     .then(() => {
         const server = express()
