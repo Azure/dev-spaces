144db04751e0ead2be10d635bedd263621969f42
Anupama Tuli <anutuli@microsoft.com>
2019-10-21 22:03:59 -0700

    Calling the class and passing the comment

--- actions/add-review-url/addComment.js
+++ actions/add-review-url/addComment.js
@@ -1,7 +1,10 @@
 const addReviewUrl = require('./lib/AddReviewUrl');
 async function run() {
     try { 
-      addReviewUrl.default;
+        const host = core.getInput('host');
+        const headref = process.env.GITHUB_HEAD_REF.toString();
+        const comment = `You can see a private version of the changes made in this pull request here:\nhttp://${headref}.s.${host}/`;
+        addReviewUrl.default.addComment(comment);
     } 
     catch (error) {
       core.setFailed(error.message);
