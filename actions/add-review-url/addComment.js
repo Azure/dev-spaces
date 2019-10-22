64c10865fe95b575edaf73b8a8510506f1c38b4f
Anupama Tuli <anutuli@microsoft.com>
2019-10-21 22:39:41 -0700

    Calling the class and passing the comment

--- actions/add-review-url/addComment.js
+++ actions/add-review-url/addComment.js
@@ -1,11 +1,11 @@
 const addReviewUrl = require('./lib/AddReviewUrl');
-const {core} = require('@actions/core');
+//const {core} = require('@actions/core');
 async function run() {
     try { 
-        const host = core.getInput('host');
-        const headref = process.env.GITHUB_HEAD_REF.toString();
-        const comment = `You can see a private version of the changes made in this pull request here:\nhttp://${headref}.s.${host}/`;
-        new addReviewUrl.default().addComment(comment);
+        //const host = core.getInput('host');
+        //const headref = process.env.GITHUB_HEAD_REF.toString();
+        //const comment = `You can see a private version of the changes made in this pull request here:\nhttp://${headref}.s.${host}/`;
+        new addReviewUrl.default().addComment("hi comment");
     } 
     catch (error) {
       core.setFailed(error.message);
