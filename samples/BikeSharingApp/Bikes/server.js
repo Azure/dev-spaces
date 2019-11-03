c50c08fdf1407873d66b036a9164d1a841518414
Anupama Tuli <anutuli@microsoft.com>
2019-11-02 17:18:38 -0700

    trig

--- samples/BikeSharingApp/Bikes/server.js
+++ samples/BikeSharingApp/Bikes/server.js
@@ -234,8 +234,7 @@ app.get('/api/bikes/:bikeId', function(req, res) {
         delete theBike._id;
 
         res.send(theBike);
-    });
-    
+    });    
 });
 
 // delete bike ------------------------------------------------------------
