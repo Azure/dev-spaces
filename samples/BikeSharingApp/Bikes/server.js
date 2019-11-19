6cb44ce86cac4816afda315807555cfef7cee750
devspaces-kubecon <57688084+devspaces-kubecon@users.noreply.github.com>
2019-11-19 13:02:43 -0800

    Update server.js

--- samples/BikeSharingApp/Bikes/server.js
+++ samples/BikeSharingApp/Bikes/server.js
@@ -230,8 +230,8 @@ app.get('/api/bikes/:bikeId', function(req, res) {
 
         var theBike = result;
         theBike.id = theBike._id;
-        delete theBike._id; 
-
+        delete theBike._id;
+  
         res.send(theBike);
     });
 });
