27c733193a91629210f2a884ae3fe39fe1680ee9
Zach Rhoads <43684271+zr-msft@users.noreply.github.com>
2020-04-13 15:02:17 -0500

    Fixed comments (#316)

--- samples/python/getting-started/webfrontend/webfrontend.py
+++ samples/python/getting-started/webfrontend/webfrontend.py
@@ -1,5 +1,5 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
 
 from flask import Flask, request, send_from_directory
 app = Flask(__name__, static_url_path='')
